e9e8c9324f0f12b8a4d405ba34d79cb3edb60971
PedroMoraes12 <pedrohenriquemoraes58@gmail.com>
2025-10-14 21:02:50 -0300

    Update TabelaHash.java
    adicionei codigo

--- TabelaHash.java
+++ TabelaHash.java
@@ -1,1 +1,9 @@
+package hash;
 
+abstract class TabelaHash {
+    int tam;
+    int contCol = 0;
+    TabelaHash(int t) { tam = t; }
+    abstract void inserir(Registro r);
+    abstract boolean buscar(String c);
+}
