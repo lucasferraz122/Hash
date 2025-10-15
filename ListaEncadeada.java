4c83e0311225055e610d840222940e27536d3368
PedroMoraes12 <pedrohenriquemoraes58@gmail.com>
2025-10-14 21:05:46 -0300

    Update ListaEncadeada.java
    adicionei codigo

--- ListaEncadeada.java
+++ ListaEncadeada.java
@@ -1,1 +1,28 @@
+package hash;
 
+class ListaEncadeada {
+    static class No {
+        Registro r;
+        No prox;
+        No(Registro rr) { r = rr; }
+    }
+
+    No inicio;
+    int tam = 0;
+
+    void inserir(Registro rr) {
+        No n = new No(rr);
+        n.prox = inicio;
+        inicio = n;
+        tam++;
+    }
+
+    boolean buscar(String c) {
+        No aux = inicio;
+        while(aux != null) {
+            if(aux.r.cod.equals(c)) return true;
+            aux = aux.prox;
+        }
+        return false;
+    }
+}
